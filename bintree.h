a5edec42b8031e80dbdd10b769068e35e13c936f
Graziella Buitron <graziella.buitron@g.austincc.edu>
2022-04-12 14:55:18 -0500

    initial commit of bintree.cpp and header file.. added comment headers

--- bintree.h
+++ bintree.h
@@ -1,6 +1,9 @@
-/*
- * Your comment header here
- */
+/******************************************************
+ * Name: Graziella Buitron
+ * Coding 07
+ * Purpose: To create a binary search tree object that
+ * will work with our struct Data and struct Node.
+ *****************************************************/
 
 #ifndef BINTREE_BINTREE_H
 #define BINTREE_BINTREE_H
