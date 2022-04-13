ae1e928c12019880f1d6d62ee8f899a7b517b4a2
Graziella Buitron <graziella.buitron@g.austincc.edu>
2022-04-13 00:42:01 -0500

    added function prototypes and attributes to bintree.h to implement bintree class

--- bintree.h
+++ bintree.h
@@ -8,7 +8,52 @@
 #ifndef BINTREE_BINTREE_H
 #define BINTREE_BINTREE_H
 
+#include <iostream>
+#include <string>
+#include "data.h"
 
+using std::cout;
+using std::endl;
+using std::string;
+
+class BinTree{
+
+public:
+    BinTree();
+    ~BinTree();
+    bool isEmpty();
+    int getCount();
+    bool getRootData(Data*);
+    void displayTree();
+
+    //public method prototypes with private overloads
+    void clear();
+    bool addNode(int, const string*);
+    bool removeNode(int);
+    bool getNode(Data*, int);
+    bool contains(int);
+    int getHeight();
+    void displayPreOrder();
+    void displayPostOrder();
+    void displayInOrder();
+
+private: 
+    //attributes
+    DataNode *root;
+    int count;
+
+    //private methods with public overloads
+    void clear(DataNode*);
+    bool addNode(DataNode*, DataNode**);
+    DataNode* removeNode(int, DataNode*);
+    bool getNode(Data*, int, DataNode*);
+    bool contains(int, DataNode*);
+    int getHeight(DataNode*);
+    void displayPreOrder(DataNode*);
+    void displayPostOrder(DataNode*);
+    void displayInOrder(DataNode*);
+
+};
 
 
 #endif /* BINTREE_BINTREE_H */
